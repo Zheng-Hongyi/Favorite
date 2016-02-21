2f49bb7a41931eb34d29f0d225b602030800888f
洪毅 郑 <hongyizheng@koutakeshis-MacBook-Pro.local>
2016-02-21 11:16:09 +0800

    初始化项目

---
+++ Favorites/Favorites/VC/FavoriteHomeVC/FavoriteHomeVC.swift
@@ -0,0 +1,35 @@
+//
+//  FavoriteHomeVC.swift
+//  Favorites
+//
+//  Created by 洪毅 郑 on 16/2/21.
+//  Copyright © 2016年 洪毅 郑. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteHomeVC: BaseTVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Favorites/Favorites/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Favorites
+//
+//  Created by 洪毅 郑 on 16/2/20.
+//  Copyright © 2016年 洪毅 郑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var optionalInteger: Int?
+    var optionalValue: Optional<Int>
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
