69594dba11e39bda55e0ebd5e93fe1dbffa67c8e
洪毅 郑 <hongyizheng@koutakeshis-MacBook-Pro.local>
2016-03-14 09:47:10 +0800

    添加缓存以及二级页面

--- Favorites/Favorites/VC/AddFavoriteItemVC/AddFavoriteCategoryVC.swift
+++ Favorites/Favorites/VC/AddFavoriteItemVC/AddFavoriteCategoryVC.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class AddFavoriteCategoryVC: BaseVC {
+class AddFavoriteCategoryVC: BaseVC, UITextFieldDelegate {
 
+    @IBOutlet weak var groupNameTextFiled: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var currentCategory : FavoriteCategory?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        groupNameTextFiled.delegate = self
+        checkValidMealName()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,17 +29,42 @@ class AddFavoriteCategoryVC: BaseVC {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Private methods
 
     @IBAction func cancelAdd(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    /*
+    
+    func checkValidMealName() {
+        let text = groupNameTextFiled.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if saveButton === sender {
+            let groupName = groupNameTextFiled.text ?? ""
+            currentCategory = FavoriteCategory(tmpName: groupName)
+        }
     }
-    */
+    
+    // MARk: UITextFiledDelegate
+    func textFieldDidBeginEditing(textField: UITextField) {
+        saveButton.enabled = false
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        checkValidMealName()
+        navigationItem.title = textField.text
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        groupNameTextFiled.resignFirstResponder()
+        return true
+    }
 
 }
--- Favorites/Favorites/VC/AddFavoriteItemVC/AddFavoriteItemVC.swift
+++ Favorites/Favorites/VC/AddFavoriteItemVC/AddFavoriteItemVC.swift
@@ -1,22 +1,24 @@
 //
-//  FavoriteGroupDetailVC.swift
+//  AddFavoriteItemVC.swift
 //  Favorites
 //
-//  Created by 洪毅 郑 on 16/3/6.
+//  Created by 洪毅 郑 on 16/3/13.
 //  Copyright © 2016年 洪毅 郑. All rights reserved.
 //
 
 import UIKit
 
-class FavoriteGroupDetailVC: BaseTVC {
-    
-    var currentGropCategory : FavoriteCategory?
+class AddFavoriteItemVC: BaseVC {
 
+    @IBOutlet weak var itemLInkTextFiled: UITextField!
+    @IBOutlet weak var itemNameTextFiled: UITextField!
+    
+    var currentItem : Favorite?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.title = currentGropCategory?.categoryName
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,16 +26,13 @@ class FavoriteGroupDetailVC: BaseTVC {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func unwindToHomeList(sender: UIStoryboardSegue) {
-//        if let sourceViewController = sender.sourceViewController as? AddFavoriteCategoryVC, needAddedCategory = sourceViewController.currentCategory {
-//            groupNames.append(needAddedCategory)
-//            tableView.reloadData()
-//            
-//        }
-    }
 
-    /*
+    @IBAction func cancelAction(sender: AnyObject) {
+        dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -41,6 +40,6 @@ class FavoriteGroupDetailVC: BaseTVC {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
